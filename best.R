674f5d945c2c2f212648ebfd448342994eaf3889
tferra <toniferra@gmail.com>
2015-08-24 22:49:43 +0200

    corrected

--- best.R
+++ best.R
@@ -2,20 +2,26 @@ best <- function(state,outcome){
   t1 <- data.frame()
   care <-  read.csv("outcome-of-care-measures.csv", colClasses = "character")
   if(!(state %in% care$State)){
-    print("invalid state")
+    stop("invalid state")
   }else{
     t1 <- care[care$State==state,]
   } 
   if (outcome == "heart attack"){
-    care[,11] <- as.numeric(care[,11])
-    return(t1[order(t1[,11]),2][1])
+    nulls <-  t1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack=="Not Available"
+    t1 <- t1[!nulls,]
+    t1[,11] <- as.numeric(t1[,11])
+    return(t1[order(t1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),2][1])
   }else if (outcome == "heart failure"){
-    care[,17] <- as.numeric(care[,17])
-    return(t1[order(t1[,17]),2][1])
+    nulls <-  t1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure=="Not Available"
+    t1 <- t1[!nulls,]
+    t1[,17] <- as.numeric(t1[,17])
+    return(t1[order(t1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),2][1])
   }else if (outcome == "pneumonia"){
-    care[,23] <- as.numeric(care[,23])
-    return(t1[order(t1[,23]),2][1])
+    nulls <-  t1$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia=="Not Available"
+    t1 <- t1[!nulls,]
+    t1$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia <- as.numeric(t1$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
+    return(t1[order(t1$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),2][1])
   }else{
-    print("invalid outcome")
+    stop("invalid outcome")
   }
 }
